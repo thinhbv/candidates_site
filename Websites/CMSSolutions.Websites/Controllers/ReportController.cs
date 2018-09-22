339e7170a821c5b518f59b0a851d17794a448353
HungVV6574 <HungVV6574@seta-asia.com.vn>
2018-09-22 18:40:26 +0700

    Update

---
+++ Websites/CMSSolutions.Websites/Controllers/ReportController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web.Mvc;
+using CMSSolutions;
+using CMSSolutions.Web.Mvc;
+using CMSSolutions.Web.Themes;
+using CMSSolutions.Web.UI.ControlForms;
+using CMSSolutions.Websites.Entities;
+using CMSSolutions.Websites.Models;
+using CMSSolutions.Websites.Services;
+using CMSSolutions.Web;
+using CMSSolutions.Web.UI.Navigation;
+using CMSSolutions.Web.Routing;
+
+namespace CMSSolutions.Websites.Controllers
+{
+	[Authorize()]
+	[Themed(IsDashboard = true)]
+	public class ReportController : BaseAdminController
+    {
+		public ReportController(IWorkContextAccessor workContextAccessor)
+			: base(workContextAccessor)
+		{
+
+		}
+
+		[Url("admin/reports")]
+		public ActionResult Index()
+		{
+			WorkContext.Breadcrumbs.Add(new Breadcrumb { Text = T("Reports"), Url = "#" });
+
+			ViewBag.Title = T("Reports");
+
+			return View();
+		}
+
+    }
+}
